71d8ac483a1572d36e93410a02b96decc0d22a5a
Jesaebe Prado <jesaebe@yahoo.com.br>
2025-07-19 10:08:23 -0300

    ajustando conexão

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,13 @@ import mongoose from "mongoose";
 import fileConnect from "../conexaoMongo.js";
 
 async function connectDB() {
-  const connect = fileConnect.replace("#banco_de_dados","JesaWebBooks");
-  mongoose.connect(connect)
+  let scriptConnection = "mongodb+srv://#user:#pass@cluster0.rojjyj1.mongodb.net/#banco_de_dados?retryWrites=true&w=majority&appName=Cluster0";
+  scriptConnection = scriptConnection.replace("#user",fileConnect.user);
+  scriptConnection = scriptConnection.replace("#pass",fileConnect.pass);
+  scriptConnection = scriptConnection.replace("#banco_de_dados",fileConnect.db);
+  console.log(scriptConnection);
+  
+  mongoose.connect(scriptConnection)
   
   return await mongoose.connection;
 }
